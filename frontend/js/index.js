6bc4d97498d3cfdee128444398db875ae166d5e1
DeliriousIdea <63449281+DeliriousIdea@users.noreply.github.com>
2024-10-05 12:51:57 -0600

    Job cards rendered

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -12,7 +12,7 @@ const searchedJobref = document.querySelector('#query-input')
 /**
  * @type {HTMLUListElement}
  */
-const searchedJobsRef = document.querySelector('#searched-jobs')
+const jobResultRef = document.querySelector('#searched-jobs')
 
 jobForm.addEventListener('submit', async (e) => {
     e.preventDefault()
@@ -25,21 +25,27 @@ jobForm.addEventListener('submit', async (e) => {
     }
 })
 
+/**
+ * 
+ * @param {Array} data Array of data to render
+ */
 function renderCards(data) {
     // Clear html and render new data
-    searchedJobref.innerHTML = ''
+    jobResultRef.innerHTML = ''
 
-    searchedJobref.insertAdjacentHTML('beforeend', `
-    <li class="job-card card my-1" style="width: 18rem;">
-    <div class="card-header">JOB COMPANY HERE</div>
-    <div class="card-body">
-        <h5 class="card-title">JOB TITLE HERE</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">JOB LOCATION HERE</h6>
-        <h6 class="card-subtitle mb-2 text-body-secondary">Posted FORMATTED JOB DATE HERE</h6>
-        <button class="btn btn-primary view-job-button" job-data-id="JOB ID HERE">View Job</button>
-    </div>
-    </li>       
-        `)
+    data.forEach((job) => {
+        jobResultRef.insertAdjacentHTML('beforeend', `
+        <li class="job-card card my-1" style="width: 18rem;">
+        <div class="card-header">${job.company}</div>
+        <div class="card-body">
+            <h5 class="card-title">${job.title}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">${job.location}</h6>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Posted ${job.date_posted}</h6>
+            <button class="btn btn-primary view-job-button" job-data-id="${job.id}">View Job</button>
+        </div>
+        </li>       
+            `)
+    })
 //  <li class="job-card card my-1" style="width: 18rem;">
 //   <div class="card-header">JOB COMPANY HERE</div>
 //   <div class="card-body">
