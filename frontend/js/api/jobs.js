5fee73c80300e5655ca380f3b85809886805c87e
DeliriousIdea <63449281+DeliriousIdea@users.noreply.github.com>
2024-10-05 13:54:13 -0600

    Get request on buttons working

--- frontend/js/api/jobs.js
+++ frontend/js/api/jobs.js
@@ -1,17 +1,33 @@
 // your code goes here.
+const endpoint = 'http://localhost:3000'
 
 const searchJobs = async (searchedJob) => {
     // const req = await fetch(`http://localhost:3000/jobs?search=${searchedJob}`)
     let data
     try {
-        const req = await fetch(`http://localhost:3000/jobs?q=${searchedJob}`)
+        const req = await fetch(`${endpoint}/jobs?q=${searchedJob}`)
         data = await req.json()
     } catch (e) {
         console.log(e)
     }
 
-    console.log(data)
     return data
 }
 
-export { searchJobs } 
+const getJobByID = async(jobID) =>  {
+    let data
+    try {
+        const req = await fetch(`${endpoint}/jobs/${jobID}`)
+        data = await req.json()
+    } catch (e) {
+        console.log(e)
+    }
+
+    return data
+}
+
+const saveJob = () => {
+
+}
+
+export { searchJobs, saveJob, getJobByID } 
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { searchJobs } from './api/jobs'
+import { searchJobs, saveJob, getJobByID } from './api/jobs'
 
 /**
  * @type {HTMLFormElement}
@@ -22,12 +22,17 @@ jobForm.addEventListener('submit', async (e) => {
     if (searchedJob !== '') {
         const data = await searchJobs(searchedJob)
         renderCards(data)
+    } else {
+        jobResultRef.innerHTML = ''
+        jobResultRef.insertAdjacentHTML('beforeend', `
+            <div class="text-dark">No Results Found</div>
+        `)
     }
 })
 
 /**
  * 
- * @param {Array} data Array of data to render
+ * @param {Array} data Array of jobs to display to the user
  */
 function renderCards(data) {
     // Clear html and render new data
@@ -43,16 +48,21 @@ function renderCards(data) {
             <h6 class="card-subtitle mb-2 text-body-secondary">Posted ${job.date_posted}</h6>
             <button class="btn btn-primary view-job-button" job-data-id="${job.id}">View Job</button>
         </div>
-        </li>       
-            `)
+        </li>
+        `)
     })
-//  <li class="job-card card my-1" style="width: 18rem;">
-//   <div class="card-header">JOB COMPANY HERE</div>
-//   <div class="card-body">
-//     <h5 class="card-title">JOB TITLE HERE</h5>
-//     <h6 class="card-subtitle mb-2 text-body-secondary">JOB LOCATION HERE</h6>
-//     <h6 class="card-subtitle mb-2 text-body-secondary">Posted FORMATTED JOB DATE HERE</h6>
-//     <button class="btn btn-primary view-job-button" job-data-id="JOB ID HERE">View Job</button>
-//   </div>
-// </li>       
+
+    // get list of all the buttons and attach an event listener foreach one
+    const job = document.querySelectorAll('.view-job-button')
+
+    job.forEach((j) => {
+        j.addEventListener(('click'), async () => {
+            test = await getJobByID(j.getAttribute('job-data-id'))
+            console.log(test)
+        })
+    })
+}
+
+function renderJobView(job) {
+
 }
