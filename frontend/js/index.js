7146c4e495865cf975e42f58958c8db4277ee166
DeliriousIdea <63449281+DeliriousIdea@users.noreply.github.com>
2024-10-05 14:05:54 -0600

    Job details rendered

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -17,7 +17,7 @@ const jobResultRef = document.querySelector('#searched-jobs')
 jobForm.addEventListener('submit', async (e) => {
     e.preventDefault()
     // Get input value and search
-    searchedJob = searchedJobref.value.toLowerCase().trim()
+    const searchedJob = searchedJobref.value.toLowerCase().trim()
 
     if (searchedJob !== '') {
         const data = await searchJobs(searchedJob)
@@ -57,12 +57,39 @@ function renderCards(data) {
 
     job.forEach((j) => {
         j.addEventListener(('click'), async () => {
-            test = await getJobByID(j.getAttribute('job-data-id'))
-            console.log(test)
+            const job = await getJobByID(j.getAttribute('job-data-id'))
+            renderJobView(job)
         })
     })
 }
 
+/**
+ * 
+ * @param {Object} job The job to render on the side view
+ */
 function renderJobView(job) {
+    const jobDetailsCardRef = document.querySelector("#job-details-card")
 
+    jobDetailsCardRef.innerHTML = ''
+    jobDetailsCardRef.insertAdjacentHTML('beforeend', `
+            <div class="card">
+            <div class="card-body">
+                <h3 class="card-title">${job.title}</h5>
+                <h4 class="card-subtitle mb-2 text-body-secondary pb-3">${job.company}</h6>
+                <h6 class="card-subtitle mb-2 text-body-secondary ">${job.location}</h6>
+                <h6 class="card-subtitle mb-2 text-body-secondary pb-3">Posted ${job.date_posted}</h6>
+            
+                <h5 class="card-subtitle mb-2">Description</h5>
+                <p class="card-text">${job.description}</p>
+                <h5 class="card-subtitle mb-2">Qualifications</h5>
+                <p class="card-text">${job.qualifications}</p>
+                <button class="btn btn-success save-job">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark" viewBox="0 0 16 16">
+                    <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" />
+                </svg>
+                Save Job
+                </button>
+            </div>
+            </div>`
+    )
 }
